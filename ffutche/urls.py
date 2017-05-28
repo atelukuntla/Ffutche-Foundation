73641d01a92db5dc33fc50269da9a197048cf041
abhishekzambre <abhishek.zambre@gmail.com>
2017-05-28 14:01:24 -0700

    First complete draft

--- ffutche/urls.py
+++ ffutche/urls.py
@@ -6,6 +6,8 @@ app_name = 'ffutche'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
 
+    url(r'^register/$', views.UserFormView.as_view(), name='register'),
+
     url(r'^donation/$', views.donation, name='donation'),
 
     url(r'^donation_submit/$', views.donation_submit, name='donation_submit'),
--- ffutche/views.py
+++ ffutche/views.py
@@ -1,6 +1,10 @@
 from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.views import generic
+from django.views.generic import View
 from .models import Donation
+from .forms import UserForm
 
 
 def index(request):
@@ -17,6 +21,7 @@ def donation(request):
         'some_value': some_value
     }
     return render(request, 'ffutche/donation.html', context)
+
 
 def donation_submit(request):
     contributor_name = request.POST['contributor_name']
@@ -36,3 +41,38 @@ def donation_submit(request):
     contribution.save()
 
     return render(request, 'ffutche/donation.html', {'message': "Thank you for the donation."})
+
+
+class UserFormView(View):
+    form_class = UserForm
+    template_name = 'ffutche/registration_form.html'
+
+    # Display blank form
+    def get(self, request):
+        form = self.form_class(None)
+        return render(request, self.template_name, {'form': form})
+
+    # Process registration form data
+    def post(self, request):
+        form = self.form_class(request.POST)
+
+        if form.is_valid():
+
+            user = form.save(commit=False)
+
+            # Clean (normalize) data
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user.set_password(password)
+            user.save()
+
+            # return user object if cred are correct
+            user = authenticate(username=username, password=password)
+
+            if user is not None:
+
+                if user.is_active:
+                    login(request, user)
+                    return redirect('music:index')
+
+        return render(request, self.template_name, {'form': form})
